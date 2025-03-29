6f4ab607c801088b9e6bb081b37962862817c30c
RafaelHub <raffiscoffe@gmail.com>
2025-03-29 22:48:09 +0000

    xadrez

--- xadrez.c
+++ xadrez.c
@@ -9,20 +9,25 @@ int main() {
     printf("1. Torre\n"); // opção torre
     printf("2. Bispo\n"); // opção bispo
     printf("3. Rainha\n"); // opção rainha
-    printf("Digite: "); // imprime uma msg, para o usuario digitar a opção
-    scanf("%d", &opcao); // armazenar o dado da opção
-
+    printf("Digite: ");
+    if (scanf("%d", &opcao) != 1) {
+        printf("Erro: Entrada inválida! Digite um número entre 1 e 3.\n");
+        return 1; // Finaliza o programa com erro
+    }
+ 
     printf("\n"); // usando quebra de linha para deixar o código mais bonito
 
     switch(opcao) // uso do switch, já que tem uma variavel em comun "opção"
     {
     case 1: // movimento da torre, 5 movimentos para a direita, usando for
+        printf("O movimento da torre > > > > >\n");
         for (int i = 1; i <= 5; i++)
         {
             printf("Direita!\n");
         }
         break;
     case 2: // movimento do bispo, 5 movimento na diagonal, na direita, usando while
+    printf("O movimento do bispo ^> ^> ^> ^> ^>\n");
         i = 1;
         while(i <= 5)
         {
@@ -34,6 +39,7 @@ int main() {
         break;
     
         case 3: // movimento da rainha, 8 movimentos na esquerda, usando do-while
+        printf("O movimento da rainha < < < < < < < <\n");
         i = 1;
         do
         {
